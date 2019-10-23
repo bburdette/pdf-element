9be60eb726d3b9f0a160922a805ec4a700444a83
Ben Burdette <bburdette@gmail.com>
2019-10-23 13:19:30 -0600

    show/hide

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,21 +1,51 @@
 module Main exposing (..)
 
+import Browser as B
 import Element as E
 import Element.Border as EB
+import Element.Input as EI
 import Html exposing (Html)
 import Html.Attributes as HA
 
 
-main : Html ()
-main =
+type alias Model =
+    { show : Bool }
+
+
+type Msg
+    = ShowHide
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ShowHide ->
+            { model | show = not model.show }
+
+
+view : Model -> Html Msg
+view model =
     E.layout
         []
     <|
         E.column []
             [ E.text "greetings from elm"
-            , E.el [ EB.width 5 ] <|
-                E.html <|
-                    Html.canvas [ HA.id "elm-canvas" ]
-                        []
+            , EI.button [] { label = E.text "show/hide", onPress = Just ShowHide }
+            , if model.show then
+                E.el [ EB.width 5 ] <|
+                    E.html <|
+                        Html.canvas [ HA.id "elm-canvas" ]
+                            []
+
+              else
+                E.none
             , E.text "look at my pdf"
             ]
+
+
+main =
+    B.sandbox
+        { init = { show = True }
+        , view = view
+        , update = update
+        }
